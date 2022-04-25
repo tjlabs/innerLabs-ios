8209150032d0c41dc3b4a8713ffb2e438d011191
Leo <92098727+tjlabs-dev@users.noreply.github.com>
2022-04-25 17:03:03 +0900

    1.0.8

--- JupiterSDK/Classes/NetworkManager.swift
+++ JupiterSDK/Classes/NetworkManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FirebaseFirestore
+//import Alamofire
 
 public class NetworkManager {
     public var documentListener: ListenerRegistration?
@@ -19,41 +20,68 @@ public class NetworkManager {
             print("decode error")
             return
         }
-//        print(dictionary)
+        //        print(dictionary)
         
         collectionListener.addDocument(data: dictionary) { error in
             completion?(error)
         }
     }
     
-//    func subscribe(id: String, completion: @escaping (Result<[Output], FirestoreError>) -> Void) {
-//        let collectionPath = "users/\(id)/output"
-//        removeListener()
-//        let collectionListener = Firestore.firestore().collection(collectionPath)
-//        documentListener = collectionListener
-//            .addSnapshotListener { snapshot, error in
-//                guard let snapshot = snapshot else {
-//                    completion(.failure(FirestoreError.firestoreError(error)))
-//                    return
-//                }
+//    func postToServer(input: Input) {
+//        let url = "https://ptsv2.com/t/cbztm-1650853155/post"
+//        var request = URLRequest(url: URL(string: url)!)
+//        request.httpMethod = "POST"
+//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//        request.timeoutInterval = 10
 //
-//                var messages = [Output]()
-//                snapshot.documentChanges.forEach { change in
-//                    switch change.type {
-//                    case .added, .modified:
-//                        do {
-//                            if let message = try change.document.data(as: Output.self) {
-//                                messages.append(message)
-//                            }
-//                        } catch {
-//                            completion(.failure(.decodedError(error)))
-//                        }
-//                    default: break
-//                    }
-//                }
-//                completion(.success(messages))
+//        // POST 로 보낼 정보
+//        let params = input
+//
+//        // httpBody 에 parameters 추가
+//        do {
+//            try request.httpBody = JSONSerialization.data(withJSONObject: params, options: [])
+//        } catch {
+//            print("http Body Error")
+//        }
+//
+//        AF.request(request).responseString { (response) in
+//            switch response.result {
+//            case .success:
+//                print("POST 성공")
+//            case .failure(let error):
+//                print("🚫 Alamofire Request Error\nCode:\(error._code), Message: \(error.errorDescription!)")
 //            }
+//        }
 //    }
+    
+    //    func subscribe(id: String, completion: @escaping (Result<[Output], FirestoreError>) -> Void) {
+    //        let collectionPath = "users/\(id)/output"
+    //        removeListener()
+    //        let collectionListener = Firestore.firestore().collection(collectionPath)
+    //        documentListener = collectionListener
+    //            .addSnapshotListener { snapshot, error in
+    //                guard let snapshot = snapshot else {
+    //                    completion(.failure(FirestoreError.firestoreError(error)))
+    //                    return
+    //                }
+    //
+    //                var messages = [Output]()
+    //                snapshot.documentChanges.forEach { change in
+    //                    switch change.type {
+    //                    case .added, .modified:
+    //                        do {
+    //                            if let message = try change.document.data(as: Output.self) {
+    //                                messages.append(message)
+    //                            }
+    //                        } catch {
+    //                            completion(.failure(.decodedError(error)))
+    //                        }
+    //                    default: break
+    //                    }
+    //                }
+    //                completion(.success(messages))
+    //            }
+    //    }
     
     func removeListener() {
         documentListener?.remove()
