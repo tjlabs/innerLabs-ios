9bc1490e710253e626f084e87ce11788c39b918f
Leo <92098727+tjlabs-dev@users.noreply.github.com>
2022-08-04 11:25:13 +0900

    1.6.1

---
+++ Example/JupiterSDK/UIColorExtension.swift
@@ -0,0 +1,30 @@
+//
+//  UIColorExtension.swift
+//  JupiterSDK_Example
+//
+//  Created by 신동현 on 2022/08/04.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.scanLocation = 1
+        
+        var rgbValue: UInt64 = 0
+        
+        scanner.scanHexInt64(&rgbValue)
+        
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+        
+        self.init(
+            red: CGFloat(r) / 0xff,
+            green: CGFloat(g) / 0xff,
+            blue: CGFloat(b) / 0xff, alpha: 1
+        )
+    }
+}
