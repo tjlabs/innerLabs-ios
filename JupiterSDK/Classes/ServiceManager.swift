cad99f35c1661867dbb06851e547f7e126688450
Leo <dev@tjlabscorp.com>
2023-11-28 01:45:46 +0900

    3.3.3

--- JupiterSDK/Classes/ServiceManager.swift
+++ JupiterSDK/Classes/ServiceManager.swift
@@ -3,7 +3,7 @@ import CoreMotion
 import UIKit
 
 public class ServiceManager: Observation {
-    public static let sdkVersion: String = "3.3.2.0"
+    public static let sdkVersion: String = "3.3.3"
     
     func tracking(input: FineLocationTrackingResult, isPast: Bool) {
         for observer in observers {
